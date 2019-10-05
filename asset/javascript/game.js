47f9734a307e5760c4382d79ef07d75c45ac7f91
Kitrick Bell <kitrickd.bell@gmail.com>
2019-10-04 22:12:53 -0500

    adding basic if function and prompt to the js file and created a sucessful function template

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -1,9 +1,9 @@
 
 //This sets a variable for the usernumber this must be declared before the randomNumber function below
 const userNumber = 10;
-
 const counter = 0
 const maxTries = 5
+
 // This generates sets the variable = to a random number
 const randomValue = Math.floor(Math.random () * userNumber );
 
@@ -14,9 +14,24 @@ console.log(randomValue);
 //const guess = prompt ("Please Pick a number between 1 and " + userNumber);
 
 // This simply state a condition for happens if the input is not equal to the random value var
-while ( guess != randomValue )  {
-    const guess = prompt ("Please Pick a number between 1 and " + userNumber);
 
-    // the += means we take the counter value which is set to 0 and add 1 to the counter var
-counter += 1 ;
+
+//read this statement as i = 0 if i is less than 10 then add one
+// what this for loop is saying is that the if statement will run over and over if you guess wrong up to ten times
+for (let i = 0; i<10; i++){
+
+    guess = prompt("What is your guess?");
+// this alone will not produce any result so we need to add a if function
+            if (guess == randomValue) {
+                console.log("Hooray");
+                break;
+            } else {
+                console.log("NOPE!!");
+            }
+
+
+//the for function will return 0-9 if we want to return 1-10 we can add one to the console.log
+//console.log(i + 1);
+
+
 }
