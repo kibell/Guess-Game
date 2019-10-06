cbb342572e6c37e1e3dae2b37e4e40a8dbae31a9
Kitrick Bell <kitrickd.bell@gmail.com>
2019-10-06 18:31:41 -0500

    added a new function in attempts to change the score on the html document

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -1,14 +1,18 @@
 
 //This sets a variable for the usernumber this must be declared before the randomNumber function below
 const userNumber = 10;
-const counter = 0
-const maxTries = 5
+let counter = 0;
+let maxTries = 0;
+let myScoreWins = 0;
+let myScoreLose = 0;
+const winner_span = document.getElementById ("winner");
 //const correctGuess 
 
 // This generates sets the variable = to a random number
 const randomValue = Math.floor(Math.random () * userNumber ) +1 ;
 
 console.log(randomValue);
+
 // In This prompt after the "string" the + usernumber will pull from the varible we set at the top
 //const guess = prompt ("Please Pick a number between 1 and " + userNumber)
 
@@ -19,26 +23,60 @@ console.log(randomValue);
 
 //read this statement as i = 0 if i is less than 10 then add one
 // what this for loop is saying is that the if statement will run over and over if you guess wrong up to ten times
+/*
+function startGame() {
 
-function guess() {
-for (let i = 0; i<10; i++){
+for (let i = 0; i <= 5; i++){
 
-  const  guess = prompt("Guess a number between 1 and" + userNumber);
-// this alone will not produce any result so we need to add a if function
-            if (guess == randomValue) {
+         const  guess = prompt("Guess a number between 1 and " + userNumber )
+         const myScoreWins = 0;
+    
+          if (guess == randomValue) {
                 console.log("Hooray");
-                //document.getElementById("Wins") = 
+                document.write( "You did it!! The magic number was " + randomValue );
+                console.log (myScoreWins);
                 break;
+
             } else {
                 console.log("NOPE!!");
-            }
-
-
-//the for function will return 0-9 if we want to return 1-10 we can add one to the console.log
-//console.log(i + 1);
-
+              
+            }  
 
 
 }
 
 }
+
+*/
+
+function startGame() {
+
+    for (let i = 0; i <= 5; i++){
+             console.log(randomValue);
+             const  guess = prompt("Guess a number between 1 and " + userNumber )
+            
+        
+              if (guess == randomValue) {
+                    console.log("Hooray");
+                   // document.write( "You did it!! The magic number was " + randomValue );
+                    wins();
+                    break
+                  //  myScoreWins ++;
+                   // console.log ( "Current Score  " + myScoreWins);
+                    
+    
+                } else {
+                    console.log("NOPE!!");
+                  
+                }  
+    
+    
+    }
+    
+    }
+
+    function wins (){
+        myScoreWins ++;
+        winner_span.innerHtml = myScoreWins;
+        
+    }
