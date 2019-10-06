359d7ecfdbcc5dcfe9dd6efe32c28dbea9cf32f2
Kitrick Bell <kitrickd.bell@gmail.com>
2019-10-06 12:41:05 -0500

    added a function for the guess button to only start when the button is clicked and changed the prompt to include a concatenate the userNumber const I set

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -3,9 +3,10 @@
 const userNumber = 10;
 const counter = 0
 const maxTries = 5
+//const correctGuess 
 
 // This generates sets the variable = to a random number
-const randomValue = Math.floor(Math.random () * userNumber );
+const randomValue = Math.floor(Math.random () * userNumber ) +1 ;
 
 console.log(randomValue);
 // In This prompt after the "string" the + usernumber will pull from the varible we set at the top
@@ -18,12 +19,15 @@ console.log(randomValue);
 
 //read this statement as i = 0 if i is less than 10 then add one
 // what this for loop is saying is that the if statement will run over and over if you guess wrong up to ten times
+
+function guess() {
 for (let i = 0; i<10; i++){
 
-    guess = prompt("What is your guess?");
+  const  guess = prompt("Guess a number between 1 and" + userNumber);
 // this alone will not produce any result so we need to add a if function
             if (guess == randomValue) {
                 console.log("Hooray");
+                //document.getElementById("Wins") = 
                 break;
             } else {
                 console.log("NOPE!!");
@@ -33,5 +37,8 @@ for (let i = 0; i<10; i++){
 //the for function will return 0-9 if we want to return 1-10 we can add one to the console.log
 //console.log(i + 1);
 
+
+
+}
 
 }
